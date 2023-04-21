b4d1e8d0c47444f792210874363bf06ca01897d4
Michael <rhift.michael@gmail.com>
2023-04-21 16:17:16 -0500

    Inlcui las vistas haciendo uso de las generic views excepto por vote

--- genshinapp/genshinapp/polls/views.py
+++ genshinapp/genshinapp/polls/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+from django.views import generic
 
-# Create your views here.
-from django.http import HttpResponse
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    return HttpResponse('Hora de conocerte aventurero')
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'Parece que aun no te decides, elige tu respuesta!' 
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))    
